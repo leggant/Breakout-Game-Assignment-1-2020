3bb06b5cda43020d31e841578b2a5e2060d775b6
leggant <61127354+leggant@users.noreply.github.com>
2020-08-31 09:00:03 +1200

    Breakout Game Base Files added to repo.  Background images added and a couple of blank class files created for the game ball and brick which need to be created.

---
+++ BreakoutGame.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace Breakout
+{
+    partial class BreakoutGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // BreakoutGame
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = global::Breakout.Properties.Resources.background1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(984, 761);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "BreakoutGame";
+            this.Padding = new System.Windows.Forms.Padding(30, 31, 30, 31);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Breakout Game";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ BreakoutGame.cs
@@ -0,0 +1,37 @@
+﻿/* 
+   Program name:        Breakout
+   Project file name:   BreakoutGame.cs
+   Author:              Anthony Legg
+   Date:	            12/08/2020
+   Language:            C#
+   Platform:            Microsoft Visual Studio 2019
+   Purpose:             This .......
+
+   Description:         .
+
+   Known Bugs:		    .
+                        
+   Additional Features: .
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Breakout
+{
+    public partial class BreakoutGame : Form
+    {
+        public BreakoutGame()
+        {
+            InitializeComponent();
+        }
+    }
+}
